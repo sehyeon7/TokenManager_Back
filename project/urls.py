67eb61185388c9ffac94b78323766b5a8fd79edb
Kim Nam Hee <103100783+kimnamheeee@users.noreply.github.com>
2023-11-03 19:21:35 +0900

    feat: project url

--- project/urls.py
+++ project/urls.py
@@ -1,2 +1,8 @@
 from django.urls import path
 from .views import ProjectListView, ProjectDetailView
+
+app_name = 'project'
+urlpatterns = [
+    path("", ProjectListView.as_view()),
+    path("<int:project_id>/", ProjectDetailView.as_view()),
+]
