d28d02541e189d814f7f35c642b672c3e0e9271f
sehyeon7 <worldjade07@gmail.com>
2023-11-10 15:06:59 +0900

    create: tokens model

--- tokens/models.py
+++ tokens/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from TokenTime.models import TokenTime
+from request.models import Request
 
 # Create your models here.
+class Tokens(models.Model):
+    token_name=models.CharField(max_length=64)
+    content=models.CharField(max_length=64)
+    tokenTime=models.ForeignKey(TokenTime, null=True, on_delete=models.CASCADE)
+    request=models.ForeignKey(Request, null=True, on_delete=models.CASCADE)
