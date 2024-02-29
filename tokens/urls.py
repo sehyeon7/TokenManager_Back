ba5a5884d1e42f5f214b236e623d1857b71a501f
sehyeon7 <worldjade07@gmail.com>
2024-02-29 15:36:33 +0900

    update: tokenExpiredView url

--- tokens/urls.py
+++ tokens/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import TokensListView, TokensDetailView
+from .views import TokensListView, TokensDetailView, TokenExpiredView
 
 app_name = 'tokens'
 urlpatterns= [
     path("", TokensListView.as_view()),
-    path("<int:token_id>/", TokensDetailView.as_view())
+    path("<int:token_id>/", TokensDetailView.as_view()),
+    path("expired/", TokenExpiredView.as_view())
 ]
